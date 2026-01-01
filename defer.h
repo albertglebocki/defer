2c8303a4bf6bef01ab7b69b0395ea9c43a032d27
Albert G. <74989982+albertglebocki@users.noreply.github.com>
2026-01-01 19:20:25 +0100

    Fixed macro naming convention and syntax issues

--- defer.h
+++ defer.h
@@ -2,32 +2,33 @@
 #define DEFER_H
 
 #ifndef _MAX_DEFERRED_BLOCKS
-    #define _MAX_DEFERRED_BLOCKS 64
+    #define _MAX_DEFERRED_BLOCKS (128)
 #endif
 
 #if defined(__GNUC__) || defined(__TINYC__)
     #define _defer_label(prefix, count) prefix ## count
 
-    #define scope(_inner_scope) do {                                                     \
+    #define _defer_scope(_inner_scope) do {                                            \
         unsigned char _defer_count = 0;                                         \
         void *_defer_stack[_MAX_DEFERRED_BLOCKS] = {0};                         \
         _inner_scope                                                            \
         _defer_clean:                                                           \
-            while (_defer_count > 0) {                                          \
+            if (_defer_count > 0) {                                          \
                 goto *_defer_stack[--_defer_count];                             \
             }                                                                   \
     } while(0)
 
-    #define _defer_block(code, count) do {                                      \
-        _defer_stack[_defer_count++] = &&_defer_label(_defer_begin_, count);    \
+    #define _defer_block(_scope, _count) do {                                    \
+        _defer_stack[_defer_count++] = &&_defer_label(_defer_begin_, _count);    \
         if (0) {                                                                \
-            _defer_label(_defer_begin_, count):                                 \
-                code;                                                           \
+            _defer_label(_defer_begin_, _count):                                 \
+                _scope;                                                         \
                 goto _defer_clean;                                              \
         }                                                                       \
     } while(0)
 
-    #define defer(block) _defer_block(block, __COUNTER__)
+    #define defer(scope) _defer_block(scope, __COUNTER__)
+    #define scope(scope) _defer_scope(scope)
 #else
     #warning "Defer library is supported only in GNUC and TINYC."
 #endif /* __GNUC__ || __TINYC__ */
